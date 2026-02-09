793b678aedff9a31ee0af112b85d8af7cd5e06f1
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2026-02-09 08:41:55 -0300

    save: l2/collabMessagesIcons.defs.ts

--- l2/collabMessagesIcons.defs.ts
+++ l2/collabMessagesIcons.defs.ts
@@ -1,2 +1,65 @@
-/// <mls fileReference="_102025_/l2/collabMessagesIcons.defs.ts" enhancement="_blank" />
+/// <mls fileReference="_102025_/l2/collabMessagesIcons.ts" enhancement="_blank" />
 
+// Do not change – automatically generated code. 
+
+export const asis: mls.defs.AsIs = {
+  "meta": {
+    "fileReference": "_102025_/l2/collabMessagesIcons.ts",
+    "componentType": "tool",
+    "componentScope": "editor"
+  },
+  "references": {
+    "imports": [
+      {
+        "ref": "lit",
+        "dependencies": [
+          {
+            "name": "svg",
+            "type": "function"
+          }
+        ]
+      }
+    ]
+  },
+  "asIs": {
+    "semantic": {
+      "generalDescription": "Collection of SVG icon definitions exported as constants using Lit",
+      "businessCapabilities": [],
+      "technicalCapabilities": [
+        "Lit SVG template literal exports"
+      ],
+      "implementedFeatures": [
+        "collab_message",
+        "collab_chevron_down",
+        "collab_chevron_right",
+        "collab_clock_static",
+        "collab_users",
+        "collab_user",
+        "collab_magnifying_glass",
+        "collab_arrow_up_long",
+        "collab_minus",
+        "collab_ban",
+        "collab_dot",
+        "collab_bell",
+        "collab_money",
+        "collab_pause",
+        "collab_clock",
+        "collab_check",
+        "collab_bug_x12",
+        "collab_play",
+        "collab_bug",
+        "collab_chevron_left",
+        "collab_gear",
+        "collab_translate",
+        "collab_circle_exclamation",
+        "collab_plus",
+        "collab_folder_tree",
+        "collab_triangle_exclamation",
+        "collab_bell_slash",
+        "collab_xmark",
+        "collab_spinner_clock"
+      ]
+    }
+  }
+}
+    
